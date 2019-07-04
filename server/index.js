54654e45c41950a1c77ff471167dc8b896a4b325
Saad Zafar <saadie.111@gmail.com>
2019-07-04 07:34:04 +0500

    deployed to heroku

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const server = express();
 
 server.use("*", (req, res) => {
-  res.sendfile(path.resolve(__dirname, "dist", "index.html"));
+  res.sendfile(path.resolve(__dirname, "dist", "mean-task", "index.html"));
 });
 
 const port = process.env.PORT || 5000;
