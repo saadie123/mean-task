c0b46cf29c549fa6bfa0f90b36ff4f898438fc3c
Saad Zafar <saadie.111@gmail.com>
2019-07-04 07:32:04 +0500

    deployed to heroku

--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
+const path = require("path");
 const express = require("express");
 
 const server = express();
+
+server.use("*", (req, res) => {
+  res.sendfile(path.resolve(__dirname, "dist", "index.html"));
+});
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`Server running on port ${port}`));
